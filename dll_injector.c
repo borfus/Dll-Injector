b9c35052b0d186347005e5249a771ddd173289a7
borfus <mrpeteyb@gmail.com>
2020-04-23 21:13:43 -0500

    Added updated dll_injector.c code

--- dll_injector.c
+++ dll_injector.c
@@ -2,62 +2,73 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 
-DWORD search_process_list_for_pid(HANDLE process_list_snapshot)
-{
+DWORD search_process_list_for_pid(HANDLE process_list, char *process_name) {
 	// must initialize dwSize to struct or Process32First will fail
-	PROCESSENTRY32 current_process_in_list = { .dwSize = sizeof(PROCESSENTRY32) };
+	PROCESSENTRY32 current_process = { .dwSize = sizeof(PROCESSENTRY32) };
 
-	if (Process32First(process_list_snapshot, &current_process_in_list))
-	{
-		do
-		{
-			if (!strcmp(current_process_in_list.szExeFile, process_name))
-			{
-				return current_process_in_list.th32ProcessID;
+	if (Process32First(process_list, &current_process)) {
+		while (Process32Next(process_list, &current_process)) {
+			if (strcmp(current_process.szExeFile, process_name) == 0) {
+				return current_process.th32ProcessID;
 			}
-		} while (Process32Next(process_list_snapshot, &current_process_in_list));
+		}
 	}
+
 	return 0;
 }
 
-DWORD get_pid(char *process_name)
-{
-	HANDLE process_list_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	if (process_list_snapshot != NULL)
-	{
-		return search_process_list_for_pid(process_list_snapshot);
+DWORD get_pid(char *process_name) {
+	HANDLE process_list = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (process_list != NULL) {
+		return search_process_list_for_pid(process_list, process_name);
 	}
+
 	return 0;
+}
+
+void inject_dll(DWORD pid, char *dll)
+{
+	HANDLE process = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, FALSE, pid);
+	if (process == INVALID_HANDLE_VALUE) {
+		printf("Failed to open PID %d, error code %d", pid, GetLastError());
+		return;
+	}
+
+	// write the dll name to memory
+	int namelen = strlen(dll) + 1;
+	LPVOID remote_string = VirtualAllocEx(process, NULL, namelen, MEM_COMMIT, PAGE_READWRITE);
+	WriteProcessMemory(process, remote_string, dll, namelen, NULL);
+
+	// get the address of LoadLibraryA()
+	HMODULE k32 = GetModuleHandleA("Kernel32.dll");
+	LPVOID func_adr = GetProcAddress(k32, "LoadLibraryA");
+
+	// create the thread
+	HANDLE thread = CreateRemoteThread(process, NULL, NULL, (LPTHREAD_START_ROUTINE)func_adr, remote_string, NULL, NULL);
+
+	printf("DLL path address: 0x%08x\n", thread);
+	
+	// let the thread finish and clean up
+	WaitForSingleObject(thread, INFINITE);
+	CloseHandle(thread);
 }
 
 int main()
 {
-	// Dll Path and Target Process ID
-	char *Dll_Path = "dll_path";
-	DWORD pid = get_pid("process_name");
+	char process[256], dll_path[256];
+	printf("Process name: ");
+	scanf("%s", process);
+	printf("DLL path: ");
+	scanf("%s", dll_path);
+
+	DWORD pid = get_pid(process);
 
 	if (pid)
 	{
-		// Handle to the target process using its pid
-		HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-
-		// Allocates memory inside the target process and sets it to the path of our dll
-		LPVOID pDll_Path = VirtualAllocEx(hProcess, 0, strlen(Dll_Path) + 1, MEM_COMMIT, PAGE_READWRITE);
-		WriteProcessMemory(hProcess, pDll_Path, (LPVOID)Dll_Path, strlen(Dll_Path) + 1, 0);
-
-		// Creates a remote thread in the target process and calls LoadLibraryA from Kernel32 to load our dll
-		HMODULE hKernel32 = GetModuleHandleA("Kernel32.dll");
-		PVOID load_library = GetProcAddress(hKernel32, "LoadLibraryA");
-		HANDLE load_thread = CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)load_library, pDll_Path, 0, 0);
-
-		// Waits for the execution of our loader thread to finish
-		WaitForSingleObject(load_thread, INFINITE);
-
-		printf("Dll path address: 0x%08x\n", (int)pDll_Path);
-
-		// Frees the dll path memory we allocated earlier
-		VirtualFreeEx(hProcess, pDll_Path, strlen(Dll_Path) + 1, MEM_RELEASE);
+		printf("PID: %d\n", pid);
+		inject_dll(pid, dll_path);
 	}
 
 	return 0;
 }
+
